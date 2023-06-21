0bd1859dbd16e465c4f15367554d8b279893032c
ramiro <ceinnova13458@innovaccion.mx>
2023-06-21 17:36:34 -0600

    Added functionality fot the hamburger menu

--- src/app/home/components/header/header.component.ts
+++ src/app/home/components/header/header.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'home-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
+  public hideMenu: boolean = true;
 
+  setHideMenu(): void {
+    this.hideMenu = !this.hideMenu;
+  }
 }
